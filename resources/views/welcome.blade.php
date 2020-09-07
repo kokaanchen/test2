ea3e5b82040bc8f26dbd56fd74430854bd6a8a0e
kokaan.chen <kokaan.chen@rocketsview.com>
2020-09-07 14:09:02 +0800

    Test21

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Test34
+                    Laravel Test345
                 </div>
 
                 <div class="links">
